1ac735778d0b90efd96aa2c87c4ab71e391ca0eb
aksha <aksha@192.168.0.102>
2022-10-18 23:49:39 +0530

    Dilesh Create Listner Class

---
+++ src/main/java/base_utility_package/Listener_class.java
@@ -0,0 +1,10 @@
+package base_utility_package;
+
+public class Listener_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Dilesh create Listner");
+	}
+
+}
