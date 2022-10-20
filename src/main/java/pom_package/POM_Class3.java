b6ee50aba85b9dedb2694f62b82f34a2ec57acc7
admin <admin@admin-PC>
2022-10-20 10:45:09 +0530

    POM_class3 created by Dilesh

--- src/main/java/pom_package/POM_Class3.java
+++ src/main/java/pom_package/POM_Class3.java
@@ -1,5 +1,28 @@
 package pom_package;
 
-public class POM_Class3 {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+public class POM_Class3 {
+	
+	@FindBy(xpath="//div[text()='Grocery']") private WebElement Grocery;
+	
+	@FindBy(xpath="//a[text()='Login']") private WebElement Login;
+	
+	@FindBy(xpath="//div[text()='Flipkart Plus Zone']") private WebElement Flipkart_Plus_Zone;
+    
+	@FindBy(xpath="//div[text()='EARNED SUPERCOINS']") private WebElement EARNED_SUPERCOINS;
+	public void Grocery() {
+		Grocery.click();
+	}
+	public void Login() {
+		Login.click();
+	}
+	public void Flipkart_Plus_Zone() {
+		 Flipkart_Plus_Zone.click();	
+	}
+	public void EARNED_SUPERCOINS () {
+		EARNED_SUPERCOINS.isDisplayed();
+	}
 }
+
